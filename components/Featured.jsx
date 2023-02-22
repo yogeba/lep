83be2d1d987c35350218a5fdc93fb3c73a9ec91c
Yogesh Bansal <yogeba@Yobes-MacBook-Air.local>
2023-02-22 20:39:11 +0530

    updated featured images

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,5 +1,5 @@
 import styles from "../styles/Featured.module.css";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { useState } from "react";
 
 const Featured = () => {
